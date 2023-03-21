111d779fb5e034ea558e4e498b5d5bfe94813ab4
EdisonGL <94578516+EdisonGL@users.noreply.github.com>
2023-03-21 09:38:15 -0500

    Creacion del CRUD de la tabla "cargos"

---
+++ app/Http/Controllers/CargosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cargo;
+
+class CargosController extends Controller
+{
+    public function index(){
+        $cargos = Cargo::all();
+        return view('cargo.index', compact('cargos'))
+    }
+}
---
+++ resources/views/cargo/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="div">
+            <div class="row">
+                <table>
+                    <thead>
+                        <th>id</th>
+                        <th>Nombre</th>
+                    </thead>
+                    <tbody>
+                        <td>{{cargos->id}}</td>
+                        <td>{{cargos->nombre}}</td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
