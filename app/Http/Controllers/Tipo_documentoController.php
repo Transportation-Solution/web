2de8e90ec9d8a88f2d0a84fe14db864569b0d212
EdisonGL <94578516+EdisonGL@users.noreply.github.com>
2023-03-21 10:48:20 -0500

    Creacion del CRUD  de la tabla "tipo_documento"

---
+++ app/Http/Controllers/Tipo_documentoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tipo_Documento;
+
+class Tipo_documentoController extends Controller
+{
+    public function index(){
+        $tipo_documentos=Tipo_Documento::all();
+        return view('tipo_documentos.index', compact('tipo_documentos'));
+    }
+}
