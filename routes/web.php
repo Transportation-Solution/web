82c9791e551ff3048a9f66bc4c2b91c433c93eef
EdisonGL <94578516+EdisonGL@users.noreply.github.com>
2023-04-20 11:35:07 -0500

    Creacion de los controladores y rutas de la tabla "Departamentos"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DepartamentosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Rutas de Departamentos
+Route::get('departamentos', [DepartamentosController::class, 'index'])->name('departamentos');
+Route::get('departamentos/crear', [DepartamentosController::class, 'create'])->name('departamentos.crear');
+Route::post('departamentos', [DepartamentosController::class, 'store'])->name('departamentos.guardar');
+Route::delete('dapartementos/{id}', [DepartamentosController::class, 'delete'])->name('departamentos.eliminar');
+Route::get('departamentos/{id}', [DepartamentosController::class, 'show'])->name('departamentos.detalles');
+Route::get('departamentos/{id}/editar', [DepartamentosController::class, 'edit'])->name('departamentos.editar');
+Route::put('dapartamentos/{id}', [DepartamentosController::class, 'update'])->name('departamentos.actualizar');
